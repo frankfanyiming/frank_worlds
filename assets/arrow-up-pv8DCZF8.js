d8ac34687a46a66390b5279ccf8686163eac5ad5
frank <frank@Mac.bbrouter>
2026-09-13 22:29:31 +0800

    Publish revision 28 mobile tips and XLands provenance

---
+++ assets/arrow-up-pv8DCZF8.js
@@ -0,0 +1,7 @@
+/*! XLands build provenance | xlands-frankfym001 | Frank / @FrankFYM001
+ * Upstream: https://github.com/frankfanyiming/frank_worlds
+ * Build: https://github.com/frankfanyiming/frank_worlds @ 5a2d00919b67eab3ac8a042b52b5e479c43539eb
+ * Original code: MIT. Third-party code retains its own licenses. See NOTICE.txt.
+ */
+
+import{y as e}from"./index-BIGm0RNm.js";var t=e(`arrow-down`,[[`path`,{d:`M12 5v14`,key:`s699le`}],[`path`,{d:`m19 12-7 7-7-7`,key:`1idqje`}]]),n=e(`arrow-up`,[[`path`,{d:`m5 12 7-7 7 7`,key:`hav0vg`}],[`path`,{d:`M12 19V5`,key:`x0mq9r`}]]);export{t as n,n as t};
